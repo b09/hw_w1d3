eaaa9954944eaab561af4f54173725ae2e03abd6
b09 <code2stack@gmail.com>
2018-03-08 01:06:21 +0000

    Section B, exercises 1 thru 6 of 9 have been completed

--- array_hashes_quiz_answers.rb
+++ array_hashes_quiz_answers.rb
@@ -1,4 +1,7 @@
 
+
+## A. Given the following data structure:
+
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 
@@ -46,10 +49,100 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
   # 8. How many ways can we return `"Falkirk High"` from the array?
 
-  stops.reverse
-
+  2
 
 
 
   # 9. Reverse the positions of the stops in the array
+
+  stops.reverse
+
+
+
   # 10. Print out all the stops using a for loop
+
+  stops.each { |s| puts s }
+
+
+
+
+## B. Given the following data structure:
+
+users = {
+  "Jonathan" => {
+    :twitter => "jonnyt",
+    :favourite_numbers => [12, 42, 75, 12, 5],
+    :home_town => "Stirling",
+    :pets => {
+      "fluffy" => :cat,
+      "fido" => :dog,
+      "spike" => :dog
+    }
+  },
+  "Erik" => {
+    :twitter => "eriksf",
+    :favourite_numbers => [8, 12, 24],
+    :home_town => "Linlithgow",
+    :pets => {
+      "nemo" => :fish,
+      "kevin" => :fish,
+      "spike" => :dog,
+      "rupert" => :parrot
+    }
+  },
+  "Avril" => {
+    :twitter => "bridgpally",
+    :favourite_numbers => [12, 14, 85, 88],
+    :home_town => "Dunbar",
+    :pets => {
+      "colin" => :snake
+    }
+  },
+}
+
+
+
+  # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+
+  users["Jonathan"][:twitter]
+
+
+
+  # 2. Get Erik's hometown
+
+  users["Erik"][:home_town]
+
+
+
+  # 3. Get the array of Erik's favourite numbers
+
+  users["Erik"][:favourite_numbers]
+
+
+
+  # 4. Get the type of Avril's pet Colin
+
+  users["Avril"][:pets]["colin"]
+
+
+
+  # 5. Get the smallest of Erik's favourite numbers
+
+  users["Erik"][:favourite_numbers].min
+
+
+
+  # 6. Add the number `7` to Erik's favourite numbers
+
+  users["Erik"][:favourite_numbers].push(7)
+
+
+
+  # 7. Change Erik's hometown to Edinburgh
+
+  
+
+
+
+  # 8. Add a pet dog to Erik called "Fluffy"
+  # 9. Add yourself to the users hash
